b06874d1c503aa446232daaa1903073c7d170baf
farrukh1212cs <50712521+farrukh1212cs@users.noreply.github.com>
2025-10-29 00:01:07 +0500

    Check in Lecture 5 Completed

---
+++ ECommerce.Infrastructure/Data/PostgresDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace ECommerce.Infrastructure.Data;
+
+public class PostgresDbContextFactory : IDesignTimeDbContextFactory<PostgresDbContext>
+{
+    public PostgresDbContext CreateDbContext(string[] args)
+    {
+        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../ECommerce.API");
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("PostgreSQL");
+
+        var optionsBuilder = new DbContextOptionsBuilder<PostgresDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new PostgresDbContext(optionsBuilder.Options);
+    }
+}
