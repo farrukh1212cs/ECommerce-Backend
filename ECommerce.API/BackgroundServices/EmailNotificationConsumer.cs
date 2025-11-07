636368a51be6ceaaedca97993da9a4b489cb08d5
farrukh1212cs <50712521+farrukh1212cs@users.noreply.github.com>
2025-11-07 12:17:34 +0500

    Check in Lesson 7 : Message Queues with RabbitMQ

---
+++ ECommerce.API/BackgroundServices/EmailNotificationConsumer.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Application.DTOs;
+using ECommerce.Application.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ECommerce.API.BackgroundServices
+{
+    public class EmailNotificationConsumer : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMessageQueueService _queue;
+
+        public EmailNotificationConsumer(IServiceScopeFactory scopeFactory, IMessageQueueService queue)
+        {
+            _scopeFactory = scopeFactory;
+            _queue = queue;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _queue.Subscribe<EmailNotificationDto>("email_notifications", async email =>
+            {
+                using var scope = _scopeFactory.CreateScope();
+                var emailSender = scope.ServiceProvider.GetRequiredService<IEmailSenderService>();
+
+                Console.WriteLine($"📧 Sending email to: {email.To}");
+                await emailSender.SendEmailAsync(email);
+            });
+
+            return Task.Delay(Timeout.Infinite, stoppingToken);
+        }
+    }
+}
